553accd718ff4bb9fd2fdfdb008ecdea4ab55540
Andreas Wachs <andreas@wachs.dk>
2021-09-03 13:53:33 +0200

    Feature/leap year function (#1)
    * Implement leap year method and tests
    
    * Add concurrency group
    
    Co-authored-by: Andreas Wachs Hjalager <ahja@itu.dk>

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,11 +2,32 @@
 
 namespace ConsoleApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+
+        public static bool IsLeapYear(int year)
+        {
+            if (year % 400 == 0)
+            {
+                return true;
+            } 
+            else if (year % 100 == 0)
+            {
+                return false;
+            } 
+            else if (year % 4 == 0)
+            {
+                return true;
+            } 
+            else
+            {
+                return false;
+            }
         }
     }
 }
