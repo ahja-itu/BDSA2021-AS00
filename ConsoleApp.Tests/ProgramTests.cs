3ae4eac5a5b1ae6f7083035d5bc6f6c9cc4108c6
Andreas Wachs Hjalager <ahja@itu.dk>
2021-09-03 14:12:09 +0200

    Add tests for user input

--- ConsoleApp.Tests/ProgramTests.cs
+++ ConsoleApp.Tests/ProgramTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace ConsoleApp.Tests
@@ -36,5 +37,55 @@ namespace ConsoleApp.Tests
             var result = Program.IsLeapYear(year);
             Assert.True(result);
         }
+
+
+
+        [Theory]
+        [InlineData(1700)]
+        [InlineData(1800)]
+        [InlineData(1900)]
+        [InlineData(1)]
+        [InlineData(-1)]
+        [InlineData(3)]
+        [InlineData(5)]
+        public void Main_inputNonLeapYear_ShouldReplyNay(int year)
+        {
+            var writer = new StringWriter();
+            var reader = new StringReader(year.ToString() + Environment.NewLine);
+
+            Console.SetOut(writer);
+            Console.SetIn(reader);
+
+            Program.Main(new string[0]);
+
+            var result = writer.GetStringBuilder().ToString();
+            Assert.Contains("nay", result);
+        }
+
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(4)]
+        [InlineData(-4)]
+        [InlineData(20)]
+        [InlineData(40)]
+        [InlineData(400)]
+        [InlineData(1200)]
+        [InlineData(2000)]
+        [InlineData(2024)]
+        public void Main_inputLeapYear_ShouldReplyYay(int year)
+        {
+            var writer = new StringWriter();
+            var reader = new StringReader(year.ToString() + Environment.NewLine);
+
+            Console.SetOut(writer);
+            Console.SetIn(reader);
+
+            Program.Main(new string[0]);
+
+            var result = writer.GetStringBuilder().ToString();
+            Assert.Contains("yay", result);
+        }
+
     }
 }
